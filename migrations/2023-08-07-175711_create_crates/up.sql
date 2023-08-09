d8c29d947b3a1d593bafbf1076d8aac89e523bb0
Alexander Chelyadinov <artma.gronsky@gmail.com>
2023-08-09 07:01:18 +0300

    D3[MAJOR]: Add diesel ORM, add migrations for new table creation

---
+++ migrations/2023-08-07-175711_create_crates/up.sql
@@ -0,0 +1,14 @@
+-- Your SQL goes here
+--  diesel migration generate create_crates
+--  diesel migration run
+--  diesel migration revert
+
+CREATE TABLE crates (
+    id SERIAL PRIMARY KEY,
+    rustacean_id integer NOT NULL REFERENCES rustaceans(id),
+    code VARCHAR(64) NOT NULL,
+    name VARCHAR(128) NOT NULL,
+    version VARCHAR(64) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL
+);
