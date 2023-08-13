bbf8b459c18858b3b5dbbd151d19f3aaa3fb91d4
Alexander Chelyadinov <artma.gronsky@gmail.com>
2023-08-13 21:42:59 +0300

    D5[MINOR]: Add migrations and models for user and roles entities.

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,7 @@
 use chrono::NaiveDateTime;
 use diesel::{Insertable, Queryable, AsChangeset};
 use rocket::serde::{Deserialize, Serialize};
-use crate::schema::{rustaceans, crates};
+use crate::schema::{rustaceans, crates, users, roles, users_roles};
 
 #[derive(Queryable, AsChangeset, Deserialize, Serialize)]
 pub struct Rustacean{
@@ -42,4 +42,52 @@ pub struct NewCrate{
     pub code: String,
     pub version: String,
     pub description: Option<String>
+}
+
+#[derive(Queryable, AsChangeset)]
+pub struct User{
+    pub id: i32,
+    pub username: String,
+    pub password: String,
+    pub created_at: NaiveDateTime    
+}
+
+#[derive(Insertable)]
+#[diesel(table_name=users)]
+pub struct NewUser{
+    pub username: String,
+    pub password: String,
+}
+
+
+
+#[derive(Queryable, AsChangeset)]
+pub struct Role{
+    pub id: i32,
+    pub code: String,
+    pub name: String,
+    pub created_at: NaiveDateTime    
+}
+
+#[derive(Insertable)]
+#[diesel(table_name=roles)]
+pub struct NewRole{
+    pub code: String,
+    pub name: String,
+}
+
+#[derive(Queryable)]
+#[diesel(belongs_to(User))]
+#[diesel(belongs_to(Role))]
+pub struct UserRole{
+    pub id: i32,
+    pub user_id:i32,
+    pub role_id: i32
+}
+
+#[derive(Insertable)]
+#[diesel(table_name=users_roles)]
+pub struct NewUserRole{
+    pub user_id:i32,
+    pub role_id: i32
 }
