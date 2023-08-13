af968909f5dc299f474a00861c99673b8eae3142
Alexander Chelyadinov <artma.gronsky@gmail.com>
2023-08-13 22:46:33 +0300

    D5[MAJOR]: Turn to a lib project, add bin for server and cli, add cli command skillet for adding users

---
+++ src/bin/server.rs
@@ -0,0 +1,40 @@
+extern crate cr8s;
+
+use dotenv::dotenv;
+use std::env;
+
+
+#[rocket::main]
+async fn main() {
+    dotenv().ok();
+ 
+    print_env_variable();
+
+    let _ = rocket::build()
+        .mount(
+            "/",
+            rocket::routes![
+                // rustaceans
+                cr8s::rocket_routes::rustaceans::get_rustacean,
+                cr8s::rocket_routes::rustaceans::get_rustaceans,
+                cr8s::rocket_routes::rustaceans::create_rustacean,
+                cr8s::rocket_routes::rustaceans::update_rustacean,
+                cr8s::rocket_routes::rustaceans::delete_rustacean,
+                // crates
+                cr8s::rocket_routes::crates::get_crates,
+                cr8s::rocket_routes::crates::get_crate,
+                cr8s::rocket_routes::crates::create,
+                cr8s::rocket_routes::crates::update,
+                cr8s::rocket_routes::crates::delete,
+            ],
+        )
+        .attach(cr8s::rocket_routes::DbConn::fairing())
+        .launch()
+        .await;
+}
+
+fn print_env_variable() {
+    for (key, value) in env::vars() {
+        println!("{}: {}", key, value);
+    }
+}
