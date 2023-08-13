59bc3bde89ab0813002f643655a8f8bb0010d35e
Alexander Chelyadinov <artma.gronsky@gmail.com>
2023-08-13 14:08:43 +0300

    D4[MINOR]: Fix field order in crate struct.

--- src/models.rs
+++ src/models.rs
@@ -1,13 +1,15 @@
 use chrono::NaiveDateTime;
 use diesel::{Insertable, Queryable, AsChangeset};
-use rocket::serde::Deserialize;
+use rocket::serde::{Deserialize, Serialize};
 use crate::schema::{rustaceans, crates};
 
-#[derive(Queryable, AsChangeset, Deserialize)]
+#[derive(Queryable, AsChangeset, Deserialize, Serialize)]
 pub struct Rustacean{
+    #[serde(skip_deserializing)]
     pub id: i32, 
     pub name: String,
     pub email: String,
+    #[serde(skip_deserializing)]
     pub created_at: NaiveDateTime,
 }
 
@@ -19,18 +21,20 @@ pub struct NewRustacean{
     pub email: String,
 }
 
-#[derive(Queryable, AsChangeset)]
+#[derive(Queryable, AsChangeset, Serialize, Deserialize)]
 pub struct Crate{
+    #[serde(skip_deserializing)]
     pub id: i32,
     pub rustacean_id: i32,
-    pub name: String,
     pub code: String,
+    pub name: String,
     pub version: String,
     pub description: Option<String>,
+    #[serde(skip_deserializing)]
     pub created_at: NaiveDateTime,
 }
 
-#[derive(Insertable)]
+#[derive(Insertable, Deserialize)]
 #[diesel(table_name=crates)]
 pub struct NewCrate{
     pub rustacean_id: i32,
